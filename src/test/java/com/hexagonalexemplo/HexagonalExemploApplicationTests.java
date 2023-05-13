3e0975e213340e7b806844c493b15c89eca14c7c
Luiz Fernando <luizfernando.silva@outlook.com>
2023-05-13 14:37:23 -0300

    Initial commit

---
+++ src/test/java/com/hexagonalexemplo/HexagonalExemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexagonalexemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalExemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
